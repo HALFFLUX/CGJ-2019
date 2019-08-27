2762ebaacb98f617ca61897f41f6263a55ab2dbc
Iuliep <50663146+Iuliep@users.noreply.github.com>
2019-08-27 13:12:41 +0300

    m-am plictisit

---
+++ CGJ-2019/Assets/Scripts/Camera/testScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    public Camera cam;
+    private bool eyesclosed = false;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && eyesclosed == false)
+        {
+            Debug.Log(cam.cullingMask.ToString());
+            cam.cullingMask = 511;
+            eyesclosed = true;
+
+
+        }
+        else if(Input.GetKeyDown(KeyCode.Space) && eyesclosed == true)
+        {
+            Debug.Log(cam.cullingMask.ToString());
+            cam.cullingMask = 567;
+            eyesclosed = false;
+        }
+    }
+}
