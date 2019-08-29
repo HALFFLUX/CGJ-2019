9e719ed14d5853d349864f148bcdfc9ab1a41f9a
Iuliep <50663146+Iuliep@users.noreply.github.com>
2019-08-29 20:57:43 +0300

    Revert "Revert "gabi gay""
    This reverts commit dd7cc925cd0bcc862b62409677371ce675c699b9.

---
+++ CGJ-2019/Assets/Scripts/onCollision.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onCollision : MonoBehaviour
+{
+    public Animator anim;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("hi");
+        anim.Play("bridgeAnim");
+        Destroy(this);
+    }
+}
