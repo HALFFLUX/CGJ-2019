dfe2fa4d2c12e000f5a641102a0dff951762f360
Iuliep <50663146+Iuliep@users.noreply.github.com>
2019-08-29 21:00:59 +0300

    Revert "Revert "gabi gay""
    This reverts commit b1cd516eb5ad6f3d57b161a9d7b2221dfbaea76c.

---
+++ CGJ-2019/Assets/Scripts/eyeScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class eyeScript : MonoBehaviour
+{
+    // false = opened, true = closed
+    public bool eyeStatus = false;
+    public GameObject particles;
+    public Animator playerAnimator;
+    public GameObject openEyes;
+    public GameObject closedEyes;
+    public Light lumina;
+    public AudioSource audio;
+    public AudioClip open;
+    public AudioClip close;
+
+    public Text eyeStatusText;
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && eyeStatus == false)
+        {
+            openEyes.SetActive(false);
+            closedEyes.SetActive(true);
+            eyeStatusText.text = "CLOSED";
+            eyeStatusText.color = Color.red;
+            lumina.intensity = .3f;
+            audio.PlayOneShot(close, 1.0f);
+            eyeStatus = true;
+            particles.SetActive(true);
+            playerAnimator.SetBool("eyesOpen", false);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && eyeStatus == true)
+        {
+            openEyes.SetActive(true);
+            closedEyes.SetActive(false);
+            eyeStatusText.text = "OPEN";
+            eyeStatusText.color = Color.green;
+            lumina.intensity = 2.3f;
+            audio.PlayOneShot(open, 1.0f);
+            eyeStatus = false;
+            particles.SetActive(false);
+            playerAnimator.SetBool("eyesOpen", true);
+        }
+    }
+}
---
+++ CGJ-2019/Assets/Scripts/rotateCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateCamera : MonoBehaviour
+{
+    public GameObject player;
+    public float speed = .1f;
+    public bool interp = false;
+    void Start()
+    {
+
+    }
+
+
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+             player.transform.eulerAngles = Vector3.Lerp(player.transform.eulerAngles, new Vector3(0, player.transform.eulerAngles.y + 180f, 0), speed);
+
+        if (Input.GetKeyDown(KeyCode.E))
+            player.transform.eulerAngles = Vector3.Lerp(player.transform.eulerAngles, new Vector3(0, player.transform.eulerAngles.y - 180f, 0), speed);
+
+    }
+
+}
+
+
